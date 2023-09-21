6b47abef79307b9dac4a0cc6bd0431610a4edaf2
danielkim-dk <danielkim.sung@gmail.com>
2023-09-21 08:23:58 -0700

    slight header changes

--- Client/src/container/BrokerContainer.jsx
+++ Client/src/container/BrokerContainer.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import BrokerCard from '../components/BrokerCard'
 
+
+/**
+ * TODO: create a map function that will take in data for broker cards and display 
+ * 
+ */
 function BrokerContainer() {
   return (
     <>
     <div className='BrokerContainer'>
       <div id='BrokerContainerTitle'>
-        <h1> Current Brokers </h1>
+        <h1> Current Brokers / Dynamic Header? </h1>
       </div>
       <section className='CardContainer'>
       <BrokerCard/>
