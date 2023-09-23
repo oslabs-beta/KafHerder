41cb1499a13d04f18aa3ea5be1be8266a871df68
Moiz Rauf <moizrauf@Moizs-Air.home>
2023-09-23 17:09:23 -0400

    created basic prometheus route that scrapes data, but only two irrelevant metrics coming back

--- server/controllers/promController.js
+++ server/controllers/promController.js
@@ -1,13 +1,20 @@
+const fetch = require('node-fetch'); // for some reason we need to do npm install node-fetch@2
+
 const promController = {}
 
 promController.getMetrics = async (req, res, next) => {
     try {
-        const response = await fetch('http://localhost/:9090');
-        const metrics = await response.json();
+        console.log('about to make request');
+        const response = await fetch('http://localhost:9090/api/v1/label/__name__/values') //('http://localhost:9090/api/v1/query?query={__name__!=""}');
+        console.log('retrieved data of type: ', typeof (response));
+        const metrics = await JSON.stringify(response); //response.json();
         res.locals.metrics = metrics;
+        console.log(metrics);
+        return next();
     }
     catch (err) {
         console.log(err);
+        next(err);
     }
 }
 
--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const promController = require('./controllers/promController.js');
 
 
