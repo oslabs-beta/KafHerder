44dfcc5de1ad527aa7396ded4fe479675945886d
Moiz Rauf <moizrauf@Moizs-Air.home>
2023-09-25 14:00:40 -0400

    used axios instead of node-fetch to solve issues with express not fetching prometheus metrics

--- server/controllers/promController.js
+++ server/controllers/promController.js
@@ -1,20 +1,27 @@
 const fetch = require('node-fetch'); // for some reason we need to do npm install node-fetch@2
+const axios = require('axios');
 
 const promController = {}
 
 promController.getMetrics = async (req, res, next) => {
-    try {
+    try {        
         console.log('about to make request');
-        const response = await fetch('http://localhost:9090/api/v1/label/__name__/values') //('http://localhost:9090/api/v1/query?query={__name__!=""}');
-        console.log('retrieved data of type: ', typeof (response));
-        const metrics = await JSON.stringify(response); //response.json();
-        res.locals.metrics = metrics;
-        console.log(metrics);
+
+        const response = await axios.get('http://localhost:9090/api/v1/label/__name__/values');
+        console.log('this is the data: ', response.data.data);
+        res.locals.metrics = response.data.data;
+
+        // const response = await fetch('http://localhost:9090/api/v1/label/__name__/values'); //('http://localhost:9090/api/v1/query?query={__name__!=""}');
+        // console.log('retrieved data of type: ', typeof (response));
+        // console.log(response);
+        // console.log(response.data);
+        // const metrics = await JSON.stringify(response); //response.json();
+
         return next();
     }
     catch (err) {
         console.log(err);
-        next(err);
+        return next(err);
     }
 }
 
