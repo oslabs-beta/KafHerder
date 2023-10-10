c16e9aa22060e30a52e10a1b664a7cffb4449933
Moiz Rauf <moizrauf@Moizs-Air.home>
2023-10-10 10:11:40 -0400

    added explanation of repartitionerAgent

--- kafkaTest/Repartitioner.js
+++ kafkaTest/Repartitioner.js
@@ -29,15 +29,27 @@ export class RepartitionerGroup {
     }
 }
 
-export class Repartitioner {
+// each RepartitionerAgent consists of one consumer reading from one partition in and old topic
+// and one producer writing to one partition in a new topic
+// the logic lies in making sure the agent only reads (and writes) to a stopping point (the first consumerOffset in the config)
+// then pauses and remains paused until ALL the agents in the RepartitionerGroup have reached their stopping points
+// then all resume in tandem to read until their next respective stopping points
+// because all partitions being read by agents in a RepartitionerGroup have the same consumer offset configurations
+// this means we are guaranteeing the exactly-once processing of messages in the new partition
+// TODO: make sure your last stopping point is the END of the partition, not the last offset
+// TODO: for the LAST consumer to pause at the nth stopping point
+// make sure it records its offset position so the future consumer knows where to pick off from
+export class RepartitionerAgent {
     constructor (props, rpGroup, oldPartitionNum, newPartitionNum, id){
-        this.props = props; // seedBrokerUrl, oldTopic, newTopicName
+        this.seedBrokerUrl = props.seedBrokerUrl;
+        this.oldTopic = props.oldTopic;
+        this.newTopicName = props.newTopicName;
+
         this.rpGroup = rpGroup; // required to access checkIfAllPaused and unpauseAll methods
         this.oldPartitionNum = oldPartitionNum;
         this.newPartitionNum = newPartitionNum;
         this.id = id;
 
-        // 
         this.isPaused;
         this.stoppingPoint;
         this.producer;
