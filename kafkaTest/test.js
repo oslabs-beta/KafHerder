24de9345144a5fb894d230c27e3d6ac8f5f7dbfe
Moiz Rauf <moizrauf@Moizs-Air.home>
2023-10-12 01:47:17 -0400

    created run.js to simplify process of testing repartitioning and creating sample data

--- kafkaTest/test.js
+++ kafkaTest/test.js
@@ -156,8 +156,16 @@ const setTestOffsets = async (topic) => {
 const sendTestMessages = async (topic, messagesToSend) => {
     const producer = kafka.producer();
     let counter = 0;
+    let interval; // Define the interval here
 
     const produceMessage = async () => {
+        if (counter >= messagesToSend) {
+            clearInterval(interval); // Accessible here
+            console.log('done sending messages!');
+            await producer.disconnect();
+            return;
+        }
+        
         console.log('animal sent');
         try {
             await producer.send({
@@ -175,14 +183,12 @@ const sendTestMessages = async (topic, messagesToSend) => {
     try {
         await producer.connect();
         console.log('sending messages...');
-        while (counter < messagesToSend){
-            setInterval(produceMessage, 10)
-        }
-        console.log('done sending messages!')
+        interval = setInterval(produceMessage, 10); // Assign the interval here
     } catch (error) {
         console.error(error);
     }
 }
+
         
 // TODO: add delete topic, create new topic, run producers as well
 const reset = async (topic) => {
@@ -212,6 +218,53 @@ const hardReset = async (topic, numPartitions, replicationFactor, messagesToSend
     catch (error) { console.error(error) }
 }
 
+module.exports = { reset, hardReset }
 
-reset('animals2');
-// hardReset('animals2', 5, 3, 500);
+
+// reset('animals2');
+// hardReset('animals2', 5, 3, 500); // do this for the FIRST
+
+// INSTRUCTIONS:
+// 1. First, cd to the kafkaTest folder
+// 2. Second, I would suggest (for consistency) deleting your docker container then rebuilding it:
+// docker-compose -f docker-compose.yml up -d
+//
+// if you get caching issues:
+// docker-compose build --no-cache kafka1
+// docker-compose build --no-cache kafka2
+// docker-compose build --no-cache kafka3
+// docker-compose up -d
+//
+// 3. Third, run node test.js. MAKE SURE hardReset('animals2', 5, 3, 500) is NOT commented out
+// this will create a topic animals2 with 5 partitions and a rep factor of 3
+// send 500 messages to the 5 partitions
+// then create 3 consumer groups to read from it, A B and C
+// in 3 different partition consumer offset configurations: 3 x ABC, 1 x BAC, and 1 x CAB
+// this is the perfect scenario to try out repartitioning
+//
+// 4. cd to ../server and npm start to run our Express server
+//
+// 5. on Postman, doa POST request to http://localhost:3000/admin/repartition
+// with the following in the body (JSON):
+/*
+{
+    "seedBrokerUrl": "localhost:9092",
+    "topicName": "animals2",
+    "newTopicName": "animals2_copy",
+    "newMinPartitionNumber": 3,
+    "newReplicationFactorNumber": 3
+}
+*/
+//
+// 6. Wait about 20 seconds while watching your Express server.
+// it may seem stuck on "Still waiting for completion", but sometimes it needs 5 seconds or so
+// at the end of it, you should see it console.log ENTIRE REPARTITIONING PROCESS HAS BEEN COMPLETED
+// and on Postman you should get an object with all the new consumer offsets
+// this object was very deliberately designed like this because I can just feed each individual property back
+// into kafkaJS to actually reset the offsets for them
+//
+// 7. If you need to retest again, you don't need to rebuild your Docker containers
+// just comment in reset("animals2") instead of hardReset, let it finish, ctrl+C
+// then run step 5 again (Postman) - JUST MAKE SURE TO CHANGE THE NEW TOPIC NAME
+// reset is necessary because repartitioning creates a lot of excess consumer groups that need to be deleted
+// I will work on middleware to delete this for us and also reconfigure their consumer groups to the new topic
